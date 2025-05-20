cc89af4776dad6f25e0f583f02a8f4558844eb93
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-05-20 21:49:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.cpp
+++ Difficulty: Easy/Count Digits/count-digits.cpp
@@ -1,12 +1,15 @@
-// User function Template for C++
-
-int countDigits(int n) {
-    int count = 0;
-    // write your code here
-    while(n>0){
-        count++;
-        n = n/10;
+class Solution {
+  public:
+    // Function to count the number of digits in n that evenly divide n
+    int evenlyDivides(int n) {
+        // code here
+        int count = 0;
+        int y = n;
+        while(n>0){
+            int x = n%10;
+            n = n/10;
+            if(x!=0 && y%x==0) count++;
+        }
+        return count;
     }
-    return count;
-    // return number of digits in n
-}
+};
