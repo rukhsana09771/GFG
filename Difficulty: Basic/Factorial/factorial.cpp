fe89dca1a10e1b452c4b10ea9bc34dc2e5e7db1a
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-05-22 15:45:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+   
+    int factorial(int n) {
+        // code here
+        int fact = 1;
+        for(int i=1; i<=n; i++){
+            fact = fact*i;
+        
+        }
+        return fact;
+    }
+};
