47feb4a6ca51f32721f76df9f9f5f67e480350ea
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-06-19 08:15:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome/palindrome.cpp
+++ Difficulty: Easy/Palindrome/palindrome.cpp
@@ -4,14 +4,16 @@ class Solution {
   public:
     bool isPalindrome(int n) {
         // Code here.
-        int num = n;
+        int x = n;
         int rev = 0;
-        while(n>0){
-            int rem = n%10;
+        int rem;
+        while(x!=0){
+            rem = x%10;
             rev = rev*10 + rem;
-            n /= 10;
+            x /= 10;
         }
-        if(num==rev) return true;
+        
+        if(n==rev) return true;
         else return false;
     }
 };
