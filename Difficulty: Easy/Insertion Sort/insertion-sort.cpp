dd73b97d072553346d83f00ef2c444252b5e6162
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-06-28 08:48:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -1,8 +1,8 @@
+/*
 class Solution {
   public:
     // Please change the array in-place
     void insertionSort(vector<int>& arr) {
-        // code here
         int n = arr.size();
         for(int i=1; i<n; i++){
             int key = arr[i];
@@ -13,5 +13,27 @@ class Solution {
             }
             arr[j+1] = key;
         }
+    }
+};
+*/
+
+
+// Recursive Insertion Sort
+class Solution {
+  public:
+    void recursiveInsertion(vector<int>& arr, int i, int n){
+            if(i==n) return;
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j]>key){
+               arr[j+1] = arr[j];
+               j--;
+            }
+            arr[j+1] = key;
+            recursiveInsertion(arr, i+1, n);
+    }
+    void insertionSort(vector<int>& arr) {
+        int n = arr.size();
+        recursiveInsertion(arr, 0, n);
     }
 };
