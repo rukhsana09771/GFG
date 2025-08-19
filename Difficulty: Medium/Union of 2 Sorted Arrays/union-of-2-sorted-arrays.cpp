9d0135bd2e5e45d92f8f2d771f35893b8cff0bcc
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-08-19 09:05:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        set<int> st;
+        for(auto i: a) st.insert(i);
+        for(auto j: b) st.insert(j);
+        vector<int> ans(st.begin(), st.end());
+        return ans;
+    }
+};
