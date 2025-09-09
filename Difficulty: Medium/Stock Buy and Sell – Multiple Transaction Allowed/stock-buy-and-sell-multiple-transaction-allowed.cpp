492725e407843f3fff5f2b4fca97e10324045db5
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-09-09 21:14:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock Buy and Sell – Multiple Transaction Allowed/stock-buy-and-sell-multiple-transaction-allowed.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &arr) {
+        // code here
+        long long profit = 0;
+        for(int i=1; i<arr.size(); i++){
+            if(arr[i]>arr[i-1]) profit += arr[i]-arr[i-1];
+        }
+        return profit;
+    }
+};
