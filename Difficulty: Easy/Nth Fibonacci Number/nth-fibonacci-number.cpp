088827b2647a9eddb8672ff192cce0c78f5ed341
Rukhsana Anjum <133005092+rukhsana09771@users.noreply.github.com>
2025-10-01 08:51:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n) {
+        // code here
+        if(n==0 || n==1) return n;
+        else return nthFibonacci(n-1) + nthFibonacci(n-2);
+    }
+};
